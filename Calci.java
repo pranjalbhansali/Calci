f10f8760c1593cbcdd2274ba4b7359b3308e35b4
PRB <pranjal.bhansali1@gmail.com>
2019-07-29 13:15:47 +0530

    Initial Commit

---
+++ Calci.java
@@ -0,0 +1,14 @@
+public class Calci{
+	public int add(int x, int y){
+		return x+y;
+	}
+	public int sub(int x, int y){
+		return x-y;
+	}
+	public int mul(int x, int y){
+		return x*y;
+	}
+	public int div(int x, int y){
+		return x/y;
+	}
+}
